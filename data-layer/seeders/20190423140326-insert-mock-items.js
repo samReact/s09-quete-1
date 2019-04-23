74c509907808f5df1a22aa361067f43751e493fb
Samir Lehmil <samir@1kubator.com>
2019-04-23 16:38:58 +0200

    Add Heroku prod app

---
+++ data-layer/seeders/20190423140326-insert-mock-items.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const items = [
+  {
+    id: "HAMMER-1234",
+    name: "Hammer"
+  },
+  {
+    id: "NAILS-5678",
+    name: "Nails"
+  }
+];
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    !process.env.PROD && queryInterface.bulkInsert("Items", items, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    !process.env.PROD &&
+      queryInterface.bulkDelete(
+        "Items",
+        {
+          id: { [Sequelize.Op.in]: items.map(item => item.id) }
+        },
+        {}
+      );
+  }
+};
