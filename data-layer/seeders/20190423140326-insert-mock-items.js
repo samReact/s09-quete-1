6e2d5fe6ae77d628749c46c46cfa4d76b275a09c
Samir Lehmil <samir@1kubator.com>
2019-04-23 16:50:14 +0200

    Test

--- data-layer/seeders/20190423140326-insert-mock-items.js
+++ data-layer/seeders/20190423140326-insert-mock-items.js
@@ -13,17 +13,20 @@ const items = [
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    !process.env.PROD && queryInterface.bulkInsert("Items", items, {});
+    process.env.PROD
+      ? queryInterface.bulkInsert("Items", {})
+      : queryInterface.bulkInsert("Items", items, {});
   },
 
   down: (queryInterface, Sequelize) => {
-    !process.env.PROD &&
-      queryInterface.bulkDelete(
-        "Items",
-        {
-          id: { [Sequelize.Op.in]: items.map(item => item.id) }
-        },
-        {}
-      );
+    process.env.PROD
+      ? queryInterface.bulkDelete(
+          "Items",
+          {
+            id: { [Sequelize.Op.in]: items.map(item => item.id) }
+          },
+          {}
+        )
+      : queryInterface.bulkDelete("Items", {});
   }
 };
