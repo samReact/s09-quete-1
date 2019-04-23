711d552f1c2d255856add5242a9d535c4869a93e
Samir Lehmil <samir@1kubator.com>
2019-04-23 16:41:33 +0200

    Test

--- data-layer/seeders/20190423131615-insert-items.js
+++ data-layer/seeders/20190423131615-insert-items.js
@@ -1,29 +1,29 @@
-// "use strict";
+"use strict";
 
-// const items = [
-//   {
-//     id: "HAMMER-1234",
-//     name: "Hammer"
-//   },
-//   {
-//     id: "NAILS-5678",
-//     name: "Nails"
-//   }
-// ];
+const items = [
+  {
+    id: "HAMMER-1234",
+    name: "Hammer"
+  },
+  {
+    id: "NAILS-5678",
+    name: "Nails"
+  }
+];
 
-// module.exports = {
-//   up: (queryInterface, Sequelize) => {
-//     !process.env.PROD && queryInterface.bulkInsert("Items", items, {});
-//   },
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    !process.env.PROD && queryInterface.bulkInsert("Items", items, {});
+  },
 
-//   down: (queryInterface, Sequelize) => {
-//     !process.env.PROD &&
-//       queryInterface.bulkDelete(
-//         "Items",
-//         {
-//           id: { [Sequelize.Op.in]: items.map(item => item.id) }
-//         },
-//         {}
-//       );
-//   }
-// };
+  down: (queryInterface, Sequelize) => {
+    !process.env.PROD &&
+      queryInterface.bulkDelete(
+        "Items",
+        {
+          id: { [Sequelize.Op.in]: items.map(item => item.id) }
+        },
+        {}
+      );
+  }
+};
